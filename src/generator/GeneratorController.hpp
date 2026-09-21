40de8f68b756eefda2fb689cb6b99117096cc1c1
cuuw619-maker <cuuw619@gmail.com>
2026-09-21 23:25:51 +0300

    Expand IMUX generator controller API

--- src/generator/GeneratorController.hpp
+++ src/generator/GeneratorController.hpp
@@ -1,6 +1,11 @@
 #pragma once
+#include "../audio/AudioTypes.hpp"
+#include "../generator/LevelGraph.hpp"
 
 class GeneratorController {
 public:
     static void show();
+    static void showSettings();
+    static imux::audio::AudioAnalysis analyze(std::vector<float> const& mono, float sampleRate);
+    static imux::generator::LevelGraph generate(imux::audio::AudioAnalysis const& analysis);
 };
