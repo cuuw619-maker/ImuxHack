05db9a390f2439ebdf7d54c86e03bd4ea5efaa2f
cuuw619-maker <cuuw619@gmail.com>
2026-09-21 23:28:55 +0300

    Fix IMUX editor popup and button integration

--- src/editor/ImuxEditorUI.cpp
+++ src/editor/ImuxEditorUI.cpp
@@ -1,64 +1,54 @@
 #include <Geode/Geode.hpp>
-#include <Geode/binding/LevelEditorLayer.hpp>
 #include <Geode/modify/EditorUI.hpp>
-#include "../generator/GeneratorController.hpp"
-
+#include "../core/Settings.hpp"
 using namespace geode::prelude;
 
-class ImuxEditorPanel {
-public:
-    static void open() {
-        auto lel = LevelEditorLayer::get();
-        auto bpm = lel ? 0.f : 0.f;
-        std::string text =
-            "IMUX EDITOR\n\n"
-            "Audio: waiting for analysis\n"
-            "BPM: --\n"
-            "Beat: --\n"
-            "Energy: --\n\n"
-            "Generation\n"
-            "Density / Sync / Difficulty are controlled by IMUX settings.\n\n"
-            "The generator works through the public IMUX API and produces a deterministic LevelGraph.";
-        FLAlertLayer::create("IMUX", text, "CLOSE")->show();
+class ImuxEditorPanel final : public FLAlertLayer {
+protected:
+    bool init() {
+        if (!FLAlertLayer::init(260.f, 190.f, "GJ_square01.png")) return false;
+        auto title = CCLabelBMFont::create("IMUX GENERATOR", "goldFont.fnt");
+        title->setScale(.65f); title->setPosition({130.f,162.f}); m_mainLayer->addChild(title);
+        auto info = CCLabelBMFont::create("Beat-driven generation core\nAudio -> beats -> LevelGraph", "bigFont.fnt");
+        info->setAlignment(kCCTextAlignmentCenter); info->setScale(.38f); info->setPosition({130.f,128.f}); m_mainLayer->addChild(info);
+        auto add = [&](char const* text, SEL_MenuHandler sel, float y) {
+            auto s = ButtonSprite::create(text, 120, true, "goldFont.fnt", "GJ_button_01.png", 0.f, 1.f);
+            auto b = CCMenuItemSpriteExtra::create(s, this, sel); b->setPosition({130.f,y}); m_buttonMenu->addChild(b);
+        };
+        add("GENERATE", menu_selector(ImuxEditorPanel::onGenerate),82.f);
+        add("SETTINGS", menu_selector(ImuxEditorPanel::onSettings),48.f);
+        add("CLOSE", menu_selector(ImuxEditorPanel::onClose),14.f);
+        return true;
     }
+public:
+    static ImuxEditorPanel* create() {
+        auto r=new ImuxEditorPanel();
+        if (r && r->init()) { r->autorelease(); return r; }
+        CC_SAFE_DELETE(r); return nullptr;
+    }
+    void onGenerate(CCObject*) {
+        auto const& s=imux::core::settings();
+        auto msg=fmt::format("Generator ready\nDifficulty: {:.2f}\nDensity: {:.2f}\nSync: {:.2f}\nSeed: {}",s.difficulty,s.density,s.syncStrength,s.seed);
+        FLAlertLayer::create("IMUX",msg.c_str(),"OK")->show();
+    }
+    void onSettings(CCObject*) { FLAlertLayer::create("IMUX SETTINGS","Use Geode mod settings to configure generation.","OK")->show(); }
+    void onClose(CCObject*) { this->keyBackClicked(); }
 };
 
 struct $modify(ImuxEditorUI, EditorUI) {
-    bool init(LevelEditorLayer* lel) {
-        if (!EditorUI::init(lel))
-            return false;
-
-        auto addButton = [this](CCMenu* menu) {
-            if (!menu || menu->getChildByID("imux-generator-button"_spr))
-                return;
-
-            auto sprite = CCSprite::createWithSpriteFrameName("GJ_plusBtn_001.png");
-            if (!sprite) {
-                log::warn("ImuxHack: editor button sprite unavailable");
-                return;
-            }
-
-            sprite->setScale(0.72f);
-            auto button = CCMenuItemSpriteExtra::create(
-                sprite, this,
-                menu_selector(ImuxEditorUI::onImuxButton)
-            );
-            button->setID("imux-generator-button"_spr);
-            menu->addChild(button);
-            menu->updateLayout();
-        };
-
-        if (auto menu = this->getChildByID("toolbar-categories-menu")) {
-            addButton(menu);
-        } else if (auto menu = this->getChildByType<CCMenu>(0)) {
-            addButton(menu);
-        }
-
-        log::info("ImuxHack: IMUX editor interface attached");
+    bool init(LevelEditorLayer* levelEditor) {
+        if (!EditorUI::init(levelEditor)) return false;
+        imux::core::load();
+        auto menu=this->getChildByID("toolbar-categories-menu");
+        if (!menu) menu=this->getChildByType<CCMenu>(0);
+        if (!menu || menu->getChildByID("imux-generator-button")) return true;
+        auto sprite=CCSprite::createWithSpriteFrameName("GJ_plusBtn_001.png");
+        if (!sprite) return true;
+        sprite->setScale(.72f);
+        auto button=CCMenuItemSpriteExtra::create(sprite,this,menu_selector(ImuxEditorUI::onImuxButton));
+        button->setID("imux-generator-button");
+        menu->addChild(button); menu->updateLayout();
         return true;
     }
-
-    void onImuxButton(CCObject*) {
-        ImuxEditorPanel::open();
-    }
+    void onImuxButton(CCObject*) { ImuxEditorPanel::create()->show(); }
 };
