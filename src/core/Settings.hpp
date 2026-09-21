71c7c75cd08564e298eb9daa9aa597698b50d134
cuuw619-maker <cuuw619@gmail.com>
2026-09-21 23:49:42 +0300

    Add source audio setting to generator config

--- src/core/Settings.hpp
+++ src/core/Settings.hpp
@@ -1,9 +1,11 @@
 #pragma once
 #include <Geode/Geode.hpp>
 #include <cstdint>
+#include <string>
 
 namespace imux::core {
 struct Settings {
+    std::string audioFile;
     float difficulty = 0.50f;
     float density = 0.65f;
     float syncStrength = 0.90f;
