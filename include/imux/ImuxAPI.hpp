43eddc322683fb130bf12de2b4753a5994e045dd
cuuw619-maker <cuuw619@gmail.com>
2026-09-24 11:54:45 +0300

    Expose stable public IMUX API header

---
+++ include/imux/ImuxAPI.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+// Stable public include for third-party Geode mods.
+// Prefer including this path instead of reaching into src/.
+
+#include "../../src/api/ImuxAPI.hpp"
+#include "../../src/integration/ThirdPartyAPI.hpp"
