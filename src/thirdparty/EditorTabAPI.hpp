6727fa89ecb63a9dc2aea410f8fbe4f0c2d08c9e
cuuw619-maker <cuuw619@gmail.com>
2026-09-25 08:59:18 +0300

    fix: add vendored EditorTab API declaration

---
+++ src/thirdparty/EditorTabAPI.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#include <Geode/cocos/base_nodes/CCNode.h>
+#include <Geode/utils/ZStringView.hpp>
+#include <Geode/binding/EditButtonBar.hpp>
+#include <Geode/loader/Event.hpp>
+#include <Geode/loader/Dispatch.hpp>
+#define MY_MOD_ID "alphalaneous.editortab_api"
+#undef DELETE
+namespace alpha::editor_tabs {
+static const std::string BUILD="robtop.geometry-dash/build";
+static const std::string EDIT="robtop.geometry-dash/edit";
+static const std::string DELETE="robtop.geometry-dash/delete";
+using CreateTab=std::function<cocos2d::CCNode*()>;
+using CreateTabIcon=std::function<cocos2d::CCNode*()>;
+using ToggleTab=std::function<void(bool,cocos2d::CCNode*)>;
+using ReloadTab=std::function<void(int,int,cocos2d::CCNode*)>;
+inline void addTab(geode::ZStringView tabID,geode::ZStringView modeID,CreateTab&& createTab,CreateTabIcon&& createIcon,ToggleTab&& toggleTab=nullptr,ReloadTab&& reloadTab=nullptr) GEODE_EVENT_EXPORT_NORES(&addTab,(tabID,modeID,std::move(createTab),std::move(createIcon),std::move(toggleTab),std::move(reloadTab)));
+inline void switchMode(geode::ZStringView modeID) GEODE_EVENT_EXPORT_NORES(&switchMode,(modeID));
+inline void switchTab(geode::ZStringView tabID) GEODE_EVENT_EXPORT_NORES(&switchTab,(tabID));
+inline void changeModeSprites(bool enabled) GEODE_EVENT_EXPORT_NORES(&changeModeSprites,(enabled));
+inline void addModeSwitchCallback(geode::Function<void(geode::ZStringView id)>&& callback,geode::Mod* mod=geode::Mod::get()) GEODE_EVENT_EXPORT_NORES(&addModeSwitchCallback,(std::move(callback),mod));
+inline void addTabSwitchCallback(geode::Function<void(geode::ZStringView id)>&& callback,geode::Mod* mod=geode::Mod::get()) GEODE_EVENT_EXPORT_NORES(&addTabSwitchCallback,(std::move(callback),mod));
+inline geode::Result<std::vector<cocos2d::CCNode*>> getAllTabs() GEODE_EVENT_EXPORT(&getAllTabs,());
+inline geode::Result<geode::ZStringView> getCurrentMode() GEODE_EVENT_EXPORT(&getCurrentMode,());
+inline geode::Result<geode::ZStringView> getCurrentTab() GEODE_EVENT_EXPORT(&getCurrentTab,());
+inline EditButtonBar* createEditButtonBar(std::span<geode::Ref<cocos2d::CCNode>> nodes) GEODE_EVENT_EXPORT_NORES(&createEditButtonBar,(nodes));
+inline void removeTab(geode::ZStringView tabID) GEODE_EVENT_EXPORT_NORES(&removeTab,(tabID));
+inline void updateTabMenu() GEODE_EVENT_EXPORT_NORES(&updateTabMenu,());
+}
